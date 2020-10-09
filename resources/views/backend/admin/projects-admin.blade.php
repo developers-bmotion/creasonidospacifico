476d77039e6674c4bcbc803a577572296bb0de1d
developers-bmotion <54631586+developers-bmotion@users.noreply.github.com>
2020-10-09 16:27:10 -0500

    estados en el subsanador

--- resources/views/backend/admin/projects-admin.blade.php
+++ resources/views/backend/admin/projects-admin.blade.php
@@ -108,7 +108,8 @@ CONTENIDO DEL MODULO PROYECTOS ADMIN
 
                         <li class="m-portlet__nav-item">
                             <h5 class="mr-2">Mostrando todos los: </h5>
-                            <span id="current_status" class="m-badge m-badge--metal m-badge--wide m-badge--rounded">{{ __('revision') }}</span>
+                            <span id="current_status"
+                                  class="m-badge m-badge--metal m-badge--wide m-badge--rounded">{{ __('revision') }}</span>
                         </li>
                         <div class="m-dropdown m-dropdown--inline  m-dropdown--arrow m-dropdown--align-right"
                              m-dropdown-toggle="hover">
@@ -125,37 +126,54 @@ CONTENIDO DEL MODULO PROYECTOS ADMIN
                                                     <span class="m-nav__section-text">{{ __('selecciona') }}</span>
                                                 </li>
                                                 <li class="m-nav__item text-center">
-                                                <span style="background-color: #9c9ca5" class="changeType w-100 btn btn-metal m-btn m-btn--pill m-btn--wide btn-sm" data-type="{{\App\Project::REVISION}}">{{ __('revision') }}</span>
+                                                    <span style="background-color: #9c9ca5"
+                                                          class="changeType w-100 btn btn-metal m-btn m-btn--pill m-btn--wide btn-sm"
+                                                          data-type="{{\App\Project::REVISION}}">{{ __('revision') }}</span>
                                                 </li>
                                                 @if(\App\User::navigation() !== "Subsanador")
-                                                <li class="m-nav__item text-center">
-                                                    <span class="changeType w-100 btn btn-brand m-btn m-btn--pill m-btn--wide btn-sm" data-type="{{\App\Project::PREAPPROVAL}}">{{ __('pre_aprovado') }}</span>
-
-                                                </li>
-                                                @endif
                                                     <li class="m-nav__item text-center">
-                                                        <span class="changeType w-100 btn btn-success m-btn m-btn--pill m-btn--wide btn-sm" data-type="{{\App\Project::APPROVAL}}">{{ __('Aprovados') }}</span>
+                                                        <span
+                                                            class="changeType w-100 btn btn-brand m-btn m-btn--pill m-btn--wide btn-sm"
+                                                            data-type="{{\App\Project::PREAPPROVAL}}">{{ __('pre_aprovado') }}</span>
+
                                                     </li>
+                                                @endif
                                                 <li class="m-nav__item text-center">
-                                                    <span style="color:white;" class="changeType w-100 btn btn-warning m-btn m-btn--pill m-btn--wide btn-sm" data-type="{{\App\Project::PENDING}}">{{ __('Pendientes') }}</span>
+                                                    <span
+                                                        class="changeType w-100 btn btn-success m-btn m-btn--pill m-btn--wide btn-sm"
+                                                        data-type="{{\App\Project::APPROVAL}}">{{ __('Aprovados') }}</span>
                                                 </li>
                                                 <li class="m-nav__item text-center">
-                                                    <span class="changeType w-100 btn btn-danger m-btn m-btn--pill m-btn--wide btn-sm" data-type="{{\App\Project::REJECTED}}">{{ __('rechazados') }}</span>
+                                                    <span style="color:white;"
+                                                          class="changeType w-100 btn btn-warning m-btn m-btn--pill m-btn--wide btn-sm"
+                                                          data-type="{{\App\Project::PENDING}}">{{ __('Pendientes') }}</span>
                                                 </li>
                                                 <li class="m-nav__item text-center">
-                                                    <span style="color:white" class="changeType w-100 btn btn-success m-btn m-btn--pill m-btn--wide btn-sm" data-type="{{\App\Project::ACEPTED}}">{{ __('Aceptados') }}</span>
+                                                    <span
+                                                        class="changeType w-100 btn btn-danger m-btn m-btn--pill m-btn--wide btn-sm"
+                                                        data-type="{{\App\Project::REJECTED}}">{{ __('rechazados') }}</span>
                                                 </li>
                                                 <li class="m-nav__item text-center">
-                                                    <span class="changeType w-100 btn btn-info m-btn m-btn--pill m-btn--wide btn-sm" data-type="{{\App\Project::REVISON_UPDATE}}">{{ __('Nueva revisión') }}</span>
+                                                    <span style="color:white"
+                                                          class="changeType w-100 btn btn-success m-btn m-btn--pill m-btn--wide btn-sm"
+                                                          data-type="{{\App\Project::ACEPTED}}">{{ __('Aceptados') }}</span>
                                                 </li>
                                                 <li class="m-nav__item text-center">
-                                                    <span class="changeType w-100 btn btn-info m-btn m-btn--pill m-btn--wide btn-sm" data-type="{{\App\Project::NOT_REMEDIED}}">{{ __('No subsanados') }}</span>
+                                                    <span
+                                                        class="changeType w-100 btn btn-info m-btn m-btn--pill m-btn--wide btn-sm"
+                                                        data-type="{{\App\Project::REVISON_UPDATE}}">{{ __('Nueva revisión') }}</span>
+                                                </li>
+                                                <li class="m-nav__item text-center">
+                                                    <span
+                                                        class="changeType w-100 btn btn-info m-btn m-btn--pill m-btn--wide btn-sm"
+                                                        data-type="{{\App\Project::NOT_REMEDIED}}">{{ __('No subsanados') }}</span>
                                                 </li>
                                                 <li class="m-nav__separator m-nav__separator--fit">
                                                 </li>
-                                                 <li class="m-nav__item">
-                                                     <span class="changeType w-100 btn btn-metal m-btn m-btn--pill m-btn--wide btn-block">{{ __('todos') }}</span>
-                                                 </li>
+                                                <li class="m-nav__item">
+                                                    <span
+                                                        class="changeType w-100 btn btn-metal m-btn m-btn--pill m-btn--wide btn-block">{{ __('todos') }}</span>
+                                                </li>
                                             </ul>
                                         </div>
                                     </div>
@@ -190,21 +208,34 @@ CONTENIDO DEL MODULO PROYECTOS ADMIN
     <script src="/backend/assets/demo/custom/crud/datatables/basic/headers.js" type="text/javascript"></script>
 
     <script>
-       var estado = getStorage('storeTipoProyecto');
-       (estado) ? $('#current_status').html('hola') : $('#current_status').html('Roro')
-       console.log(estado);
+
+        var listStatus = {
+            revision: {name: 'En Revisión', color: '#9c9ca5'},
+            aprobados: {name: 'Aprobados', color: '#34bfa3'},
+            pendientes: {name: 'Pendientes', color: '#ffb822'},
+            rechazados: {name: 'Rechazados', color: '#f4516c'},
+            aceptados: {name: 'Aceptados', color: '#34bfa3'},
+            nuevarevision: {name: 'Nueva Revición', color: '#36a3f7'},
+            nosubsanados: {name: 'No Subsanados', color: '#36a3f7'},
+        }
+        var estado = getStorage('storeTipoProyecto');
+
+        if (estado){
+            changedStatusColor(parseInt(estado))
+        }
+        console.log(estado);
         var storeTipoProyecto = "storeTipoProyecto";
         var tipoProyecto = getStorage(storeTipoProyecto);
         var table = null;
-        const loadTable = function (){
-            if (table !== null){
+        const loadTable = function () {
+            if (table !== null) {
                 table.destroy();
             }
             table = $('#table_projects_admin').DataTable({
                 "processing": true,
                 "serverSide": true,
                 "data": null,
-                "order": [[ 0, "desc" ]],
+                "order": [[0, "desc"]],
                 "ajax": {
                     url: "{{route('datatables.projects.admin')}}",
                     data: {
@@ -220,11 +251,11 @@ CONTENIDO DEL MODULO PROYECTOS ADMIN
                     {
                         data: 'artists.users.name',
                         defaultContent: '<span class="label label-danger text-center" style="color:red !important">{{ __('nigun_valor_defecto') }}</span>',
-                        render : function (data, type, JsonResultRow, meta){
+                        render: function (data, type, JsonResultRow, meta) {
                             let artista = JsonResultRow.artists[0];
                             //console.log(JsonResultRow);
                             //if (JsonResultRow.status+"" === 4+""){
-                                return `<span target="_blank">${artista.users.name} ${artista.users.last_name}</span>`;
+                            return `<span target="_blank">${artista.users.name} ${artista.users.last_name}</span>`;
                             //}
                             //return artista.nickname;
                         }
@@ -238,11 +269,12 @@ CONTENIDO DEL MODULO PROYECTOS ADMIN
                         defaultContent: '<span class="label label-danger text-center" style="color:red !important">{{ __('nigun_valor_defecto') }}</span>'
                     },
 
-                    {"width": "15%",
+                    {
+                        "width": "15%",
                         data: 'status',
-                        render:function (data) {
+                        render: function (data) {
                             let info = '<span class="m-badge m-badge--danger m-badge--wide">Hola</span>';
-                            switch(parseInt(data)){
+                            switch (parseInt(data)) {
                                 case 1:
                                     info = '<span class="m-badge m-badge--brand m-badge--wide" style="background-color:#C4C5D4 !important" >{{ __('revision') }}</span>';
                                     break;
@@ -265,12 +297,12 @@ CONTENIDO DEL MODULO PROYECTOS ADMIN
                                     info = '<span class="m-badge  m-badge--success m-badge--wide">Aceptado</span>';
                                     break;
                             }
-                            return '<div class="text-center">'+info+'</div>';
-                    }
-                },
-                {
-                    render:function (data,type, JsonResultRow,meta) {
-                        return '<div class="text-center"><a href="/dashboard/project/'+JsonResultRow.slug+'" class="btn m-btn--pill btn-secondary"><i class="fa fa-eye"></i></a></div>'
+                            return '<div class="text-center">' + info + '</div>';
+                        }
+                    },
+                    {
+                        render: function (data, type, JsonResultRow, meta) {
+                            return '<div class="text-center"><a href="/dashboard/project/' + JsonResultRow.slug + '" class="btn m-btn--pill btn-secondary"><i class="fa fa-eye"></i></a></div>'
                         }
                     },
                 ],
@@ -301,35 +333,53 @@ CONTENIDO DEL MODULO PROYECTOS ADMIN
             });
         };
 
-
-        var prueba = {
-            one: {name: 'hola', color: '#fff'},
-            two: {name: 'bebe', color: '#000'}
-        }
-
-        $(".selectType").on('click', '.changeType', function(){
-            console.log('objet: ', prueba)
+        $(".selectType").on('click', '.changeType', function () {
             let tipo = parseInt($(this).attr("data-type"));
-            if (!(tipo >  0)){
+            if (!(tipo > 0)) {
                 tipo = null;
             }
             tipoProyecto = tipo;
             setStorage(storeTipoProyecto, tipoProyecto)
             loadTable();
-
-            switch (tipoProyecto) {
-                    case 0:
-                        $("#current_status").css("background", prueba.two.color);
-                        $('#current_status').html(prueba.two.name)
-                    break;
-                    case 3:
-                        console.log(prueba.two.color)
-                        $("#current_status").css("background", prueba.two.color);
-                        $('#current_status').html(prueba.two.name)
-                    break;
-                default: console.log('no entro')
-            }
+            console.log(typeof(tipoProyecto))
+            changedStatusColor(tipoProyecto)
         });
+        function changedStatusColor(status){
+            console.log(status)
+            console.log(typeof(status));
+            switch (status) {
+                case 1:
+                    $("#current_status").css("background", listStatus.revision.color);
+                    $('#current_status').html(listStatus.revision.name)
+                    break;
+                case 3:
+                    $("#current_status").css("background", listStatus.aprobados.color);
+                    $('#current_status').html(listStatus.aprobados.name)
+                    break;
+                case 4:
+                    $("#current_status").css("background", listStatus.pendientes.color);
+                    $('#current_status').html(listStatus.pendientes.name)
+                    break;
+                case 5:
+                    $("#current_status").css("background", listStatus.rechazados.color);
+                    $('#current_status').html(listStatus.rechazados.name)
+                    break;
+                case 6:
+                    $("#current_status").css("background", listStatus.nuevarevision.color);
+                    $('#current_status').html(listStatus.nuevarevision.name)
+                    break;
+                case 7:
+                    $("#current_status").css("background", listStatus.aceptados.color);
+                    $('#current_status').html(listStatus.aceptados.name)
+                    break;
+                case 8:
+                    $("#current_status").css("background", listStatus.nosubsanados.color);
+                    $('#current_status').html(listStatus.nosubsanados.name)
+                    break;
+                default:
+                    console.log('no entro')
+            }
+        }
         loadTable();
     </script>
 
