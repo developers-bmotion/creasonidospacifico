ec51226d5e4dd1a05a3d8ba9b0d4815ce013261c
developers-bmotion <54631586+developers-bmotion@users.noreply.github.com>
2020-10-29 10:23:17 -0500

    actuara

--- resources/js/components/admin/AspirantsAdmin.vue
+++ resources/js/components/admin/AspirantsAdmin.vue
@@ -90,7 +90,7 @@ export default {
                     field: "name",
                 },
                 {
-                    label: "Actuara como",
+                    label: "Actuará como",
                     field: "artista.person_type.name",
                 },
                 {
