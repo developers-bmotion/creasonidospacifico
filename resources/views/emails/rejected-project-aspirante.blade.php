b64020bff50f8401b1955312d3a2c20554dc892a
developers-bmotion <54631586+developers-bmotion@users.noreply.github.com>
2020-11-11 11:55:28 -0500

    termino revision subsanador

--- resources/views/emails/rejected-project-aspirante.blade.php
+++ resources/views/emails/rejected-project-aspirante.blade.php
@@ -1,7 +1,7 @@
 @component('mail::message')
 # ¡Saludos!  Sr/Sra aspirante {{ $name }} {{ $last_name }}.
 
-Informamos que su proyecto ha sido rechazado por realizar las correciones a tiempo.
+Informamos que su proyecto ha sido rechazado por no realizar las correciones a tiempo.
 
 {{ __('Gracias') }}.<br>
 {{ config('app.name') }}
