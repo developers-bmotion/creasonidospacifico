bc4b267b58b1e3a7bc662724899195b364af6cc6
developers-bmotion <54631586+developers-bmotion@users.noreply.github.com>
2020-11-12 15:38:12 -0500

    tarjetas dinamicas

--- resources/views/backend/admin/projects-admin.blade.php
+++ resources/views/backend/admin/projects-admin.blade.php
@@ -99,102 +99,15 @@ CONTENIDO DEL MODULO PROYECTOS ADMIN
 ======================================-->
 @section('content')
 
-    <div class="m-portlet mt-5">
-        <div class="m-portlet__body  m-portlet__body--no-padding">
-            <div class="row m-row--no-padding m-row--col-separator-xl">
-                <!--=====================================
-		            TARJETA USUARIOR REGISTRADOS
-                ======================================-->
-                <div class="col-md-12 col-lg-5 col-xl-2">
-                    <div class="m-widget24 pb-5">
-                        <div class="m-widget24__item">
-                            <h4 class="m-widget24__title">
-                                Aspirantes Registrados
-                            </h4><br>
-                            <span class="m-widget24__desc">Aspirantes con canción</span><br><br>
-                            <span class="m-widget24__stats m--font-black pull-right pl-4" style="float: left">300</span>
+    <!--=====================================
+       TARJETAS CON LA CANTIDAD DE PROYECTOS
+    ======================================-->
+    @include('backend.partials.cards-count-projects')
 
-                        </div>
-                    </div>
-                </div>
-                <!--=====================================
-                       TARJETA USUARIOR PENDIENTES
-               ======================================-->
-                <div class="col-md-12 col-lg-5 col-xl-2">
-                    <div class="m-widget24 pb-5">
-                        <div class="m-widget24__item">
-                            <h4 class="m-widget24__title">
-                                Propuestas Pendientes
-                            </h4><br>
-                            <span class="m-widget24__desc">Enviadas a revisión</span><br><br>
-                            <span class="m-widget24__stats m--font-warning pull-right pl-4" style="float: left">300</span>
+    <!--=====================================
+        TABLA CON LA LISTA DE PROPUESTAS MUSICALES
+    ======================================-->
 
-                        </div>
-                    </div>
-                </div>
-                <!--=====================================
-                        TARJETA USUARIOR NUEVA REVISIÓN
-                ======================================-->
-                <div class="col-md-12 col-lg-5 col-xl-2">
-                    <div class="m-widget24 pb-5">
-                        <div class="m-widget24__item">
-                            <h4 class="m-widget24__title">
-                                Nueva Revisión
-                            </h4><br>
-                            <span class="m-widget24__desc">Corregidas por los aspirantes</span><br><br>
-                            <span class="m-widget24__stats m--font-info pull-right pl-4" style="float: left">300</span>
-
-                        </div>
-                    </div>
-                </div>
-                <!--=====================================
-		            TARJETA USUARIOs ACEPTADOS
-                ======================================-->
-                <div class="col-md-12 col-lg-5 col-xl-2">
-                    <div class="m-widget24 pb-5">
-                        <div class="m-widget24__item">
-                            <h4 class="m-widget24__title">
-                                Propuestas Aceptadas
-                            </h4><br>
-                            <span class="m-widget24__desc">En proceso de curaduria</span><br><br>
-                            <span class="m-widget24__stats m--font-brand pull-right pl-4" style="float: left">300</span>
-
-                        </div>
-                    </div>
-                </div>
-                <!--=====================================
-		            TARJETA USUARIOs NO SUBSANADOS
-                ======================================-->
-                <div class="col-md-12 col-lg-5 col-xl-2">
-                    <div class="m-widget24 pb-5">
-                        <div class="m-widget24__item">
-                            <h4 class="m-widget24__title">
-                                No Subsanados
-                            </h4><br>
-                            <span class="m-widget24__desc">No subsanada o corregidas</span><br><br>
-                            <span class="m-widget24__stats m--font-danger pull-right pl-4" style="float: left">300</span>
-
-                        </div>
-                    </div>
-                </div>
-                <!--=====================================
-		            TARJETA USUARIOs  APROBADAS
-                ======================================-->
-                <div class="col-md-12 col-lg-5 col-xl-2">
-                    <div class="m-widget24 pb-5">
-                        <div class="m-widget24__item">
-                            <h4 class="m-widget24__title">
-                                Aprobadas
-                            </h4><br>
-                            <span class="m-widget24__desc">Aprobadas puntaje</span><br><br>
-                            <span class="m-widget24__stats m--font-success pull-right pl-4" style="float: left">300</span>
-
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
     <div class="m-portlet m-portlet--mobile mt-3">
         <div class="m-portlet__head">
             <div class="m-portlet__head-caption">
