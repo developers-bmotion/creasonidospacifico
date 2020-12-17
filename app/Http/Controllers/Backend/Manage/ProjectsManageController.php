e9630c36cb748a5fa7870d8f825cb903aab7ed39
developers-bmotion <54631586+developers-bmotion@users.noreply.github.com>
2020-12-17 11:29:52 -0500

    se arreglo la calificacion

--- app/Http/Controllers/Backend/Manage/ProjectsManageController.php
+++ app/Http/Controllers/Backend/Manage/ProjectsManageController.php
@@ -57,6 +57,31 @@ class ProjectsManageController extends Controller
         return '{"status":200, "msg":"Propuesta musical calificada"}';
     }
 
+    public function add_review_second(Request $request){
+
+        // return $request;
+        // dd('holas');
+        $review = new SecondStage();
+        $review->project_id = $request->idProject;
+        $review->user_id = auth()->user()->id;
+        $review->lyric = $request->criterio_4; //Calidad del repertorio escogido:
+        $review->melody_rhythm = $request->criterio_1; //Aspectos técnicos musicales:
+        $review->arrangements = $request->criterio_3; //Calidad interpretativa:
+        $review->originality = $request->criterio_2; //aporte creativo
+        $review->trajectory = $request->criterio_5; //trayectoria
+        $review->project_interest = $request->criterio_6; //interes comercial del proyecto
+        $review->comment = $request->comment;
+        $review->save();
+
+        // return $request;
+
+        Project::where('id', $request->idProject)->update([
+            'status' => 2
+        ]);
+
+        return '{"status":200, "msg":"Propuesta musical calificada"}';
+    }
+
     public function history_review($id){
         $reviews = Review::where('project_id', $id)->with('projects.category')->get();
         return response()->json($reviews);
